6e62a1bdb43a944d6f260ad6ac425d5bcda0a2e8
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-11 17:57:45 +0100

    [FEAT] Added Move command

---
+++ Daddabbo/T2SGameSolution/T2SGameInput/Move.cs
@@ -0,0 +1,22 @@
+namespace T2SGameInput
+{
+    /// <summary>
+    /// This class is used to handle all kind of move related commands 
+    /// which differs only in the direction of the movement.
+    /// </summary>
+    public class Move : ICommand
+    {
+        private readonly Directions _direction;
+
+        public Move(Directions direction)
+        {
+            _direction = direction;
+        }
+
+        /// <inheritdoc />
+        public void execute(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
