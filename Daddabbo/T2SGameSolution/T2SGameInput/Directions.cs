a4c7b6ca6f7c70267a2a680ceb10afb169c7ba02
Nicolò D'Addabbo <nicolo.daddabbo@studio.unibo.it>
2023-02-11 17:19:42 +0100

    [FEAT] Added Directions enum

---
+++ Daddabbo/T2SGameSolution/T2SGameInput/Directions.cs
@@ -0,0 +1,29 @@
+namespace T2SGameInput
+{
+    /// <summary>
+    /// Possible entity's movement directions.
+    /// </summary>
+    public enum Directions
+    {
+        /// <summary>
+        /// Up direction.
+        /// </summary>
+        UP,
+        /// <summary>
+        /// Right direction.
+        /// </summary>
+        RIGHT,
+        /// <summary>
+        /// Down direction.
+        /// </summary>
+        DOWN,
+        /// <summary>
+        /// Left direction.
+        /// </summary>
+        LEFT,
+        /// <summary>
+        /// Stay still direction.
+        /// </summary>
+        STAY
+    }
+}
