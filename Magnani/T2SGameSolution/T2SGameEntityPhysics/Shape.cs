98ba17170e2b2df8cd105a3fee3c10f4c6a7bc9a
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-16 14:33:38 +0100

    [FEAT] Added Shape constructor

--- Magnani/T2SGameSolution/T2SGameEntityPhysics/Shape.cs
+++ Magnani/T2SGameSolution/T2SGameEntityPhysics/Shape.cs
@@ -3,5 +3,9 @@ namespace T2SGameEntityPhysics
     public class Shape
     {
         public Vector2D Center { get; set; }
+
+        public Shape(Vector2D center){
+            Center = center;
+        }
     }
 }
