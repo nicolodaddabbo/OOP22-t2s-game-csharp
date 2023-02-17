80a424192e8f2a1549c121270bcd97a25a7f650d
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-17 17:09:19 +0100

    [DOCS] Added docs to Shape

--- Magnani/T2SGameSolution/T2SGameEntityPhysics/codeForTest/Shape.cs
+++ Magnani/T2SGameSolution/T2SGameEntityPhysics/codeForTest/Shape.cs
@@ -1,14 +1,29 @@
 namespace T2SGameEntityPhysics
 {
+    /// <summary>
+    /// This interface models a shape usefull for managing collisions.
+    /// </summary>
     public class Shape
     {
+        /// <summary>
+        /// The center of the shape.
+        /// </summary>
         public Vector2D Center { get; set; }
 
-        public Shape(Vector2D center){
+        /// <param name="center">The center of the shape.</param>
+        public Shape(Vector2D center)
+        {
             Center = center;
         }
 
-        public Boolean IsColliding(Shape shape){
+        /// <summary>
+        /// Checking if the current shape is colliding with the given shape.
+        /// </summary>
+        /// <param name="shape">The other shape to check if the current is colliding.
+        /// </param>
+        /// <returns>True if collision has been detected, otherwise false.</returns>
+        public Boolean IsColliding(Shape shape)
+        {
             return Center == shape.Center;
         }
 
