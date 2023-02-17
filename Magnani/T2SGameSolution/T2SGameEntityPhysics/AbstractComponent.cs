254f6bc6202098a6239f86e0ff45d405e609095f
Leonardo David Matteo Magnani <leonardo.magnani6@studio.unibo.it>
2023-02-17 17:49:15 +0100

    [FIX] Supressed some warnings that cannot occur

--- Magnani/T2SGameSolution/T2SGameEntityPhysics/AbstractComponent.cs
+++ Magnani/T2SGameSolution/T2SGameEntityPhysics/AbstractComponent.cs
@@ -14,6 +14,11 @@ namespace T2SGameEntityPhysics
         /// <inheritdoc />
         public abstract void Receive<T>(MessageFunc<T> message);
 
+        // Disable "non-nullable property is uninitialized" warning because an
+        // AbstractComponent always has an associated entity. The entity is assigned
+        // when the component is added to the entity, and can be accessed only through
+        // the entity, ensuring that the property is never null.
+        #pragma warning disable CS8618
         /// <summary>
         /// The entity the component is linked to.
         /// </summary>
